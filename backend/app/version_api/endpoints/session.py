2f1a0a34a7882c7af0055c3fd8890e24d5e57a82
GuetchArnaud <arnaud.guetch@gmail.com>
2023-10-28 22:19:10 +0200

    suppression du DevContenair et création du endpoint pour l'api

--- backend/app/version_api/endpoints/session.py
+++ backend/app/version_api/endpoints/session.py
@@ -1,11 +1,15 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import Any
+from datetime import timedelta
 
 router = APIRouter(
-    title= "MLops_Data"
+    title="MLops_Data",
+    description="Cette API est destinée à gerer des informations sur les données mise à disposition.",
+    version="1.0.1"
 )
 
-""" 
-    We can use this template to build the session of our API !!
-"""
+@router.post("/logging/")
+async def get_post():
+    """_summary_
+    """
